0a8d4169fa00add4f32538c654f5ddc30873e0d5
violaivanova <violett.ivanova@gmail.com>
2021-03-03 17:29:22 +0100

    parsing json

--- art/script.js
+++ art/script.js
@@ -1,3 +1,20 @@
-var json = "images.json"
-var myImages = JSON.parse(json);
-console.log(myImages[0].imgPath);
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send(); 
+}
+
+// this requests the file and executes a callback with the parsed result once
+//   it is available
+fetchJSONFile('p/Users/violaivanova/Documents/Art_Gallery_Website/astromechanic.github.io/astromechanic.github.io/art/images.json', function(data){
+    // do something with your data
+    console.log(data);
+});
